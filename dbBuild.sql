1fb58ef2e51bbd03c6561604744be033ba867994
Hakan-Kocaman <hakankocaman2020@gmail.com>
2026-02-06 23:40:53 +0300

    Add Total_price column to Orders table
    Added Total_price column to Orders table.

--- dbBuild.sql
+++ dbBuild.sql
@@ -41,6 +41,7 @@ create table Orders(
 Orders_id int identity primary key,
 Create_date date,
 Employee_id int,
+Total_price decimal(10,2), 
 );
 
 create table OrderItem(
@@ -68,3 +69,4 @@ alter table OrderItem
 
 alter table OrderItem
  add foreign key (Orders_id) references Orders(Orders_id);
+
